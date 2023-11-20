389c27665efae644bc021d084974e89669a14bc5
annovcoer <annovcoer4de@gmail.com>
2023-11-20 11:04:00 +0700

    seeders

--- database/migrations/2023_11_14_080213_create_camps_table.php
+++ database/migrations/2023_11_14_080213_create_camps_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('camps', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('title',100);
             $table->string('slug',100);
             $table->integer('price')->unsigned();
---
+++ database/seeders/CampBenefitTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\CampBenefit;
+
+class CampBenefitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $campBenefits = [
+            [
+                'camp_id'   => 1,
+                'name'      => 'Pro Techstack Kit'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'iMac Pro 2021 & Display'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'Final Project Certifikate'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'Offline Course Video'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'Future Job Opportinity'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'Premium Design Kit'
+            ],
+            [
+                'camp_id'   => 1,
+                'name'      => 'Website Builder'
+            ],
+            [
+                'camp_id'   => 2,
+                'name'      => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id'   => 2,
+                'name'      => 'Final Project Certifikate'
+            ],
+            [
+                'camp_id'   => 2,
+                'name'      => 'Offline Course Video'
+            ]
+            ];
+            CampBenefit::insert($campBenefits);
+    }
+}
